db71f60859f6477422aa7f5156bc3ee6ddf1333f
DYesca <dayescaco@est.utn.ac.cr>
2025-04-20 18:37:00 -0600

    Commit inicial

---
+++ src/views/Proyectos.vue
@@ -0,0 +1,31 @@
+<template
+    lang="html">
+    <ion-page>
+
+       <!-- Proyecto ligado a x usuario en especifico -->
+        <ion-header>
+        <ion-toolbar>
+            <ion-icon aria-hidden="true" :icon="bookOutline"></ion-icon>
+            <ion-title>Proyecto name </ion-title>
+        </ion-toolbar>
+        </ion-header>
+
+
+        <ion-content :fullscreen="true">
+     
+        <ion-list>
+            <ion-item v-for="(proyecto, index) in proyectos" :key="index">
+            <ion-label>{{ proyecto }}</ion-label>
+            </ion-item>
+        </ion-list>
+        </ion-content>
+    </ion-page>
+</template>
+<script setup lang="ts">
+
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonIcon } from '@ionic/vue';
+import { ref } from 'vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+import { bookOutline} from 'ionicons/icons';
+
+</script>
--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -2,22 +2,53 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Gestión de Proyectos</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
+
+      <!-- Lista de proyectos ligados a usuarios -->
+
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">Gestión de Proyectos</ion-title>
         </ion-toolbar>
       </ion-header>
-
-      <ExploreContainer name="Tab 1 page" />
+      <ion-list>
+        <!--mostrar proyectos -->
+        <ion-item v-for="project in projects" :key="project.id">
+          <IonLabel>{{ project.id }}</IonLabel>
+          <ion-label>{{ project.name }}</ion-label>
+        </ion-item>
+      </ion-list>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel,} from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
+import { ref, onMounted } from 'vue';
+
+// Variable reactiva para almacenar los proyectos
+const projects = ref([]);
+
+// Función para obtener los proyectos desde el API
+const fetchProjects = async () => {
+  try {
+    const response = await fetch('http://127.0.0.1:8000/api/project/list');
+    if (!response.ok) {
+      throw new Error('Error al obtener los proyectos');
+    }
+    const data = await response.json();
+    projects.value = data; // Asigna los datos a la variable reactiva
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+// Llama a la función fetchProjects cuando el componente se monta
+onMounted(() => {
+  fetchProjects();
+});
 </script>
